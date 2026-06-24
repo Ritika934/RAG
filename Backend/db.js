c3b1f16d865b47b3905a3725bd712904ffac1c75
Ritika Parmar <ritikaparmar9878@gmail.com>
2026-06-25 01:59:52 +0530

    Prepare backend for deployment

--- Backend/db.js
+++ Backend/db.js
@@ -7,7 +7,11 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 5432,
+  //for the neon 
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
